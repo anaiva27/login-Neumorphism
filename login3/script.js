43f1c5b95c27b0ddf8a26631f26558a45ecbe9fb
codespherebysam <skhan20203636@gmail.com>
2025-04-09 12:54:05 +0530

    Add files via upload

---
+++ login3/script.js
@@ -0,0 +1,26 @@
+const toggle = document.getElementById("formToggle");
+    const loginForm = document.getElementById("loginForm");
+    const registerForm = document.getElementById("registerForm");
+    const themeToggle = document.getElementById("themeToggle");
+    const toast = document.getElementById("toast");
+
+    toggle.addEventListener("change", () => {
+      loginForm.classList.toggle("active");
+      registerForm.classList.toggle("active");
+    });
+
+    document.querySelectorAll("form").forEach(form => {
+      form.addEventListener("submit", e => {
+        e.preventDefault();
+        toast.style.display = "block";
+        setTimeout(() => toast.style.display = "none", 2000);
+      });
+    });
+
+    themeToggle.addEventListener("change", () => {
+      if (document.body.getAttribute("data-theme") === "dark") {
+        document.body.removeAttribute("data-theme");
+      } else {
+        document.body.setAttribute("data-theme", "dark");
+      }
+    });
